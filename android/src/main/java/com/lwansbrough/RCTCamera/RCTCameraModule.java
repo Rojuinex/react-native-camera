4f1e8b558dede87abb2cb1db642d700f101ec207
Caleb Bartholomew <rojuinex@gmail.com>
2018-10-31 08:46:16 -0600

    More to fix upside down camrea.

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraModule.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraModule.java
@@ -250,6 +250,26 @@ public class RCTCameraModule extends ReactContextBaseJavaModule
     }
 
     /**
+     * Calculate camera rotation
+     *
+     * This calculation is applied to the output JPEG either via Exif Orientation tag
+     * or by actually transforming the bitmap. (Determined by vendor camera API implementation)
+     *
+     * Note: This is not the same calculation as the display orientation
+     *
+     * @param screenOrientationDegrees Screen orientation in degrees
+     * @return Number of degrees to rotate image in order for it to view correctly.
+     */
+    private int calcCameraRotation(int screenOrientationDegrees) {
+        if (mCameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            return (mCameraInfo.orientation + screenOrientationDegrees) % 360;
+        } else {  // back-facing
+            final int landscapeFlip = isLandscape(screenOrientationDegrees) ? 180 : 0;
+            return (mCameraInfo.orientation + screenOrientationDegrees + landscapeFlip) % 360;
+        }
+    }
+
+    /**
      * Prepare media recorder for video capture.
      *
      * See "Capturing Videos" at https://developer.android.com/guide/topics/media/camera.html for
